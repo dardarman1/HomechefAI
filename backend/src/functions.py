557ec6123ffb5c3a2894b9bb3caf2deecd68ee8b
dardarman1 <64745732+dardarman1@users.noreply.github.com>
2025-02-22 00:15:23 -0500

    Created some testing functions
    Created testing modules to test functions.py and also changed the functionality of functions.py, to store a singular Gemini API Key

--- backend/src/functions.py
+++ backend/src/functions.py
@@ -2,23 +2,41 @@ from firebase_config import get_db
 
 db = get_db
 
-# chatgpt_key refers to the ChatGPT API key
-def store_user_data(user_id, name, chatgpt_key):
-    doc_ref = db.collection('users').document(user_id)
-    user_data = {
-        'name': name,
-        'chatgpt_key': chatgpt_key
+# api_key refers to the Gemini API key
+def store_api_key(api_key):
+    """
+    _safely stores the api_key_
+
+    Args:
+        api_key (_str_): _the gemini api_key being used_
+
+    Returns:
+        _int_: _returns the int 0 if success, -1 if it fails_
+    """
+    doc_ref = db.collection('api_keys').document('gemini')
+    data = {
+        'api_key': api_key
     }
     try:
-        doc_ref.set(user_data)
+        doc_ref.set(data)
     except Exception as e:
         return -1
     return 0
 
-def get_user_data(user_id):
-    doc_ref = db.collection('users').document(user_id)
+def get_api_key():
+    """
+    Retrieves the Gemini API key from the database
+
+    Returns:
+        _int_: _returns the int -1 if error_
+        _dict_: _returns the dictionary containing the API key_
+    """
+    doc_ref = db.collection('api_keys').document('gemini')
     try:
         doc = doc_ref.get()
+        
+        if not getattr(doc, 'exists', False):
+            return -1
         return doc.to_dict()
     except Exception as e:
         return -1
