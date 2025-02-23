23ef1df6211720997d7a1ffca37b2093b29e3bed
Kaya Tacer <ktacer@proton.me>
2025-02-23 06:25:36 -0500

    recipe

--- Assets/Scripts/CreateButton.cs
+++ Assets/Scripts/CreateButton.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class CreateButton : MonoBehaviour
 {
@@ -7,6 +10,15 @@ public class CreateButton : MonoBehaviour
     public Animator recipesContentAnimator;
     public GameObject recipePrefab;
     public Transform recipesContainer;
+
+    [Serializable]
+    public class RecipeJSON
+    {
+        public string name;
+        public string url;
+        public string[] ingredients;
+        public string[] directions;
+    }
 
     public void Create() {
         List<string> ingredients = new List<string>();
@@ -18,5 +30,33 @@ public class CreateButton : MonoBehaviour
         if (ingredients.Count == 0) return;
 
         recipesContentAnimator.SetTrigger("Toggle");
+
+
+    }
+
+    IEnumerator GetRecipes(List<string> ingredients) {
+        string url = "https://my-service-894665829957.us-central1.run.app/get_recipes";
+        string ingredientsStr = string.Join(",", ingredients);
+        using (UnityWebRequest www = UnityWebRequest.Post(url, $"{{\"ingredients\": {ingredientsStr}}}", "application/json"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.downloadHandler.text);
+            }
+            else
+            {
+                string responseText = www.downloadHandler.text;
+                Debug.Log(responseText);
+
+                RecipeJSON[] recipes = JsonUtility.FromJson<RecipeJSON[]>(responseText);
+                foreach (RecipeJSON recipe in recipes)
+                {
+                    GameObject recipeObj = Instantiate(recipePrefab, recipesContainer);
+                    recipeObj.GetComponent<Recipe>().SetRecipeEntry(recipe.name, recipe.url, recipe.ingredients, recipe.directions);
+                }
+            }
+        }
     }
 }
