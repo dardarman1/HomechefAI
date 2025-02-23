e975f143fbb49ae2de68611529c63a5b4fe43a69
Siddharth Kashyap <kashya23@purdue.edu>
2025-02-22 22:08:32 -0500

    Delete connect_to_supercook.py
    delete supercook related files

--- backend/src/connect_to_supercook.py
+++
@@ -1,7 +0,0 @@
-def get_ingredients_for_supercook(ingredient_list):
-    #temp
-    return ingredient_list
-
-if __name__ == "__main__":
-    list1 = ['ham', 'eggs']
-    get_ingredients_for_supercook(list1)
